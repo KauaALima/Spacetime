66fdb2a15b8ede5ce7bfd550d93dac9584ad6ecf
Kauã Araujo de Lima <lkauan462@gmail.com>
2024-04-24 19:58:14 +0000

    App finalizado :tada:

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,13 +1,34 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
+
+app.register(multipart);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true,
 });
 
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 const port = 3333;
